71e230e4ffd28f1ab429200e54b3a7207c9bb800
MikeFranco <mfranco_98@yahoo.com>
2020-07-05 13:17:43 -0500

    added getters of title and description

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getEasyChallenges = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto(excersicesUrl, { waitUntil: 'networkidle2' });
+  await page.goto(excersicesUrl, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
   const btnSelector = 'button.ui.fluid.button';
 
   /* while((await page.$(btnSelector)) != null){
@@ -16,13 +16,23 @@ const getEasyChallenges = async () => {
   } */
 
   const getExampleInfo = await page.evaluate(() => {
-    const getRandomNumb = Math.floor(Math.random() * 10000);
+    const randomNumOfExercise = Math.floor(Math.random() * 20);
     const allLinks = Array.from(document.querySelectorAll('a.content')).map(element => element.href);
     const allLevels = Array.from(document.querySelectorAll('a.content > div > span')).map(span => span.innerText);
-    return { getRandomNumb, allLevels, allLinks };
+    return { randomNumOfExercise, allLevels, allLinks };
   });
 
-  console.log('%c⧭', 'color: #0088cc', getExampleInfo);
+  const { randomNumOfExercise, allLinks, allLevels } = getExampleInfo;
+  await page.goto(allLinks[randomNumOfExercise], { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
+  await page.waitForSelector('h2.content');
+
+  const getExcerciseInfo = await page.evaluate(() => {
+      const title = document.querySelector('h2.content').textContent;
+      const description = document.querySelectorAll('div[class="grey-segment code-area instructions"] > div')[1].innerText.split('\n').join(' ');
+      return { title, description}
+  })
+
+  console.log('%c⧭', 'color: #0088cc', getExcerciseInfo, allLinks[randomNumOfExercise]);
   console.log('%c⧭', 'color: #e50000', 'finished');
   await browser.close();
 };
