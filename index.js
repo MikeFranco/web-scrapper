81cee17c50ac3ab24e2afa2d4a196e43eb269108
MikeFranco <mfranco_98@yahoo.com>
2020-07-05 21:19:55 -0500

    init get array of 7 challenges

--- index.js
+++ index.js
@@ -14,23 +14,26 @@ const getChallengesInfo = async () => {
     page.click(btnSelector);
     await page.waitFor(2000);
   } */
-
-  const getExampleInfo = await page.evaluate(() => {
-    const randomNumOfExercise = Math.floor(Math.random() * 20);
+  
+  //const sevenTests = Array(7).fill({}).reduce((carry, element) => )
+  const getAllChallenges = await page.evaluate(() => {
     const allLinks = Array.from(document.querySelectorAll('a.content')).map(element => element.href);
     const allLevels = Array.from(document.querySelectorAll('a.content > div > span')).map(span => span.innerText);
-    return { randomNumOfExercise, allLevels, allLinks };
+    return { allLevels, allLinks };
   });
 
-  const { randomNumOfExercise, allLinks, allLevels } = getExampleInfo;
+  const randomNumOfExercise = Math.floor(Math.random() * 10000);
+
+  const { allLinks, allLevels } = getAllChallenges;
   await page.goto(allLinks[randomNumOfExercise], { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
   await page.waitForSelector('h2.content');
 
   const getExcerciseInfo = await page.evaluate(() => {
-      const title = document.querySelector('h2.content').textContent;
-      const description = document.querySelectorAll('div[class="grey-segment code-area instructions"] > div')[1].innerText.split('\n').join(' ');
-      return { title, description }
+    const title = document.querySelector('h2.content').textContent;
+    const description = document.querySelectorAll('div[class="grey-segment code-area instructions"] > div')[1].innerText.split('\n').join(' ');
+    return { title, description }
   })
+  
   //To click in Code tab
   await page.$$eval('div.rc-tabs-tab > span:nth-child(1)', anchors => {
     anchors.map(anchor => {
@@ -40,8 +43,17 @@ const getChallengesInfo = async () => {
       }
     })
   })
-
-  console.log('%c⧭', 'color: #0088cc', getExcerciseInfo, allLinks[randomNumOfExercise]);
+  
+  const getChallengeCode = await page.evaluate(() => {
+    const codesArea = Array.from(document.querySelectorAll('pre.CodeMirror-line')).map(el => el.innerText);
+    const mainFunctionCode = codesArea.filter((element, index) => index < 3);
+    const testsCode = codesArea.filter((element, index) => index >= 3);
+    return { mainFunctionCode, testsCode }
+  })
+  
+  
+  console.log('%c⧭', 'color: #00a3cc', getExcerciseInfo);
+  console.log('%c⧭', 'color: #00e600', getChallengeCode, allLinks[randomNumOfExercise]);
   console.log('%c⧭', 'color: #e50000', 'finished');
   await browser.close();
 };
